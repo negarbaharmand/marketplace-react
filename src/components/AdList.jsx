811ad9295b18cf267866adf4ca3b63a53ac62e54
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-28 15:20:55 +0100

    Fix stylings

--- src/components/AdList.jsx
+++ src/components/AdList.jsx
@@ -4,6 +4,9 @@ import AdCard from "./AdCard";
 function AdList({ advertisements }) {
   return (
     <div id="advertisements" className="row">
+      <div className="m-4">
+        <h3>Advertisement List</h3>
+      </div>
       {advertisements.map((advertisement) => (
         <AdCard key={advertisement.title} advertisement={advertisement} />
       ))}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,32 +3,45 @@ import logo from "../assets/images/blocket.jpg";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        <a class="navbar-brand" href="#">
-          <img src={logo} alt="Blocket logo" style={{ width: "120px" }} />
-        </a>
-      </div>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">
-              Login
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Sign up
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              About
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav class="navbar navbar-expand-sm navbar-light bg-white">
+        <div>
+          <a class="navbar-brand" href="#">
+            <img src={logo} alt="Blocket logo" style={{ width: "120px" }} />
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Sign up
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <banner className="h-100">
+        <div
+          className="text-bg-danger row align-items-center"
+          style={{
+            height: "120px",
+            paddingLeft: "40px",
+          }}
+        >
+          Marketplace UI!
+        </div>
+      </banner>
+    </>
   );
 }
 
