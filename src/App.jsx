12006bff00727615eefe701fc5aa8f2b05440051
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-28 10:11:27 +0100

    Add cards data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import AdList from "./components/AdList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const advertisementsData = [
+    {
+      title: "Ad 1",
+      description: "This is the first advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact1@example.com",
+    },
+    {
+      title: "Ad 2",
+      description: "This is the second advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact2@example.com",
+    },
+    {
+      title: "Ad 3",
+      description: "This is the third advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact3@example.com",
+    },
+    {
+      title: "Ad 4",
+      description: "This is the fourth advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact4@example.com",
+    },
+    {
+      title: "Ad 5",
+      description: "This is the fifth advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact5@example.com",
+    },
+    {
+      title: "Ad 6",
+      description: "This is the sixth advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact6@example.com",
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h2>Marketpalce React App</h2>
+      <AdList advertisements={advertisementsData} />
+    </div>
+  );
 }
 
-export default App
+export default App;
